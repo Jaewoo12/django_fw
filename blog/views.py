9a8937c1106c926efed69d3691ff1a8812d062e0
Jaewoo <jaewoo.jeongg@gmail.com>
2021-01-13 14:50:44 +0900

    commit2

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.utils import timezone
+from .models import Post
 
-# Create your views here.
+# Post 목록
+def post_list_response(request):
+    name = 'Django'
+    #return HttpResponse(f'''<h2>Hello {name}!!</h2><p>HTTP METHOD : {request.method}</p>''')
+    response = HttpResponse(content_type="text/html")
+    response.write(f'<h2>hello {name}!!</h2>')
+    response.write(f'<p>HTTP METHOD : {request.method}</p>')
+    response.write(f'<p>HTTP contenttype : {request.content_type}</p>')
+    return response
+
+# Post 목록2
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html',{'posts':posts})
